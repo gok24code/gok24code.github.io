5299c993153bf425dd20de07d23208f3f46ff4a0
Göktuğ <noxonninen@gmail.com>
2025-09-07 02:14:54 +0300

    index of
    some product update

---
+++ script.js
@@ -0,0 +1,34 @@
+function startCountdown() {
+  const display = document.querySelector("#count-number");
+  const timeInput = document.querySelector('input[type="time"]').value;
+  if (!timeInput) return;
+
+  let [hours, minutes] = timeInput.split(":").map(Number);
+  let totalSeconds = hours * 3600 + minutes * 60;
+
+  let timer = setInterval(() => {
+    if (totalSeconds <= 0) {
+      clearInterval(timer);
+      location.reload();
+      return;
+    }
+
+    let remainingHours = Math.floor(totalSeconds / 3600);
+    let remainingMinutes = Math.floor((totalSeconds % 3600) / 60);
+    let remainingSeconds = totalSeconds % 60;
+
+    display.textContent = `${String(remainingHours).padStart(2, "0")}:${String(
+      remainingMinutes
+    ).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+
+    totalSeconds--;
+  }, 1000);
+
+  // Buton ve inputu gizle
+  document.getElementById("startButton").style.display = "none";
+  document.getElementById("Tinput").style.display = "none";
+}
+
+document
+  .getElementById("startButton")
+  .addEventListener("click", startCountdown);
