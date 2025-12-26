83c974264452c0a50318f33ed2801bd61d989444
gok24code <noxonninen@gmail.com>
2025-12-26 04:57:54 +0300

    Revert incorrect build fixes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react({
-    // Allow JSX syntax in .js files
-    include: '**/*.{js,jsx,ts,tsx}',
-  })],
+  plugins: [react()],
 })
